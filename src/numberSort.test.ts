061d4d467bdbf267b92d82496158c3966181e738
Tim Ji <timji@kktv.me>
2024-01-31 14:21:12 +0800

    numberSort

--- src/numberSort.test.ts
+++ src/numberSort.test.ts
@@ -1,10 +1,10 @@
-import { describe, it, expect } from "vitest";
-import { sortNumbers } from "./numberSort";
+import { describe, expect, it } from 'vitest'
+import { sortNumbers } from './numberSort'
 
-describe("數字處理", () => {
-  it("應能將給定的數字陣列進行排序", () => {
+describe('數字處理', () => {
+  it('應能將給定的數字陣列進行排序', () => {
     expect(sortNumbers([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5])).toEqual([
       1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9,
-    ]);
-  });
-});
+    ])
+  })
+})
