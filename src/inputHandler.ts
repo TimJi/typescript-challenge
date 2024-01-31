f632a990cd72bde42d2c97e77ed781e7f27b85d6
Tim Ji <timji@kktv.me>
2024-01-31 14:32:30 +0800

    inputHandler

--- src/inputHandler.ts
+++ src/inputHandler.ts
@@ -11,6 +11,10 @@
  * 輸出: 'Input is a number: 123'
  */
 
-export function handleInput(input) {
+export function handleInput(input: string | number): string {
   // 在此實現函式
+  if (typeof input === 'number') {
+    return `Input is a number: ${input}`
+  }
+  return `Input is a string: ${input}`
 }
