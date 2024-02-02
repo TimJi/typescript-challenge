9d7682d09fd087cfee07be3270e7f570a8bf6081
Tim Ji <timji@kktv.me>
2024-02-02 18:02:05 +0800

    filterByProperty

--- src/filterByProperty.ts
+++ src/filterByProperty.ts
@@ -10,16 +10,9 @@
  * @param value - 要過濾的屬性值
  * @returns - 回傳過濾後的陣列
  */
-export function filterByProperty(array: [], property: string, value: any): [] {
+export function filterByProperty<T, K extends keyof T>(array: T[], property: K, value: T[K]): T[] {
   // 請在此處寫下你的程式碼
-  let result: [] = []
-  for (let i = 0; i < array.length; i++) {
-    if (array[i][property] === value) {
-      result.push(array[i])
-    }
-  }
-  return result
-
+  return array.filter((item) => item[property] === value)
 }
 
 // 寫法推薦
